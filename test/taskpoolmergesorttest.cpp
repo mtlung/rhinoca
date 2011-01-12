83bc6b3c18262d15d15b2233ef4ec8797a1e6706
ricky <ricky@localhost>
2011-01-12 12:31:35 +0800

    Improved performance of merge sort in TaskPool test

--- test/taskpoolmergesorttest.cpp
+++ test/taskpoolmergesorttest.cpp
@@ -4,21 +4,38 @@
 #include <stdlib.h>
 #include <vector>
 
-static const rhuint testSize = 10000;
+static const bool benchmark = true;
+static const rhuint testSize = benchmark ? 10000000 : 1000;
 
-static void merge(int* data, int left, int leftEnd, int right, int rightEnd)
+/// scratch is a memory buffer having the same size as data
+static void merge(int* data, int* scratch, int left, int leftEnd, int right, int rightEnd)
 {
-	// Insert the right data into correct location in the left
-	for(;left < rightEnd && right < rightEnd; ++left) {
-		int val = data[right];
-		if(left >= right || val >= data[left]) continue;
-		memmove(data + left + 1, data + left, sizeof(int) * (right - left));
-		data[left] = val;
-		++right;
+	// The merge operation is much faster with a scratch memory buffer
+	if(scratch) {
+		int begin = left;
+		int end = rightEnd;
+		for(int i=begin; i<end; ++i) {
+			if(right >= rightEnd || (left < leftEnd && data[left] <= data[right]))
+				scratch[i] = data[left++];
+			else
+				scratch[i] = data[right++];
+		}
+		for(int i=begin; i<end; ++i)
+			data[i] = scratch[i];
+	}
+	// Otherwise we fallback to a memmove solution
+	else {
+		for(;left < rightEnd && right < rightEnd; ++left) {
+			int val = data[right];
+			if(left >= right || val >= data[left]) continue;
+			memmove(data + left + 1, data + left, sizeof(int) * (right - left));
+			data[left] = val;
+			++right;
+		}
 	}
 }
 
-static void mergeSortSingleThread(int* data, int begin, int end)
+static void mergeSortSingleThread(int* data, int* scratch, int begin, int end)
 {
 	int middle = (end - begin) / 2;
 	if(middle == 0) return;
@@ -28,31 +45,32 @@ static void mergeSortSingleThread(int* data, int begin, int end)
 	int right = leftEnd;
 	int rightEnd = end;
 
-	mergeSortSingleThread(data, left, leftEnd);
+	mergeSortSingleThread(data, scratch, left, leftEnd);
 
-	mergeSortSingleThread(data, right, rightEnd);
+	mergeSortSingleThread(data, scratch, right, rightEnd);
 
-	merge(data, left, leftEnd, right, rightEnd);
+	merge(data, scratch, left, leftEnd, right, rightEnd);
 }
 
 TEST(TaskPoolSingleThreadMergeSortTest)
 {
-	std::vector<int> data(testSize);
+	std::vector<int> data(testSize), scratch(testSize);
 	for(rhuint i=0; i<data.size(); ++i)
 		data[i] = rand() % data.size();
 
 	Timer timer;
 	float begin = timer.seconds();
-	mergeSortSingleThread(&data[0], 0, data.size());
+	mergeSortSingleThread(&data[0], &scratch[0], 0, data.size());
 	float dt =  timer.seconds() - begin;
-//	printf("Time for SingleThreadMergeSort: %f\n", dt);
-	(void)dt;
+
+	if(benchmark)
+		printf("Time for SingleThreadMergeSort: %f\n", dt);
 
 	for(rhuint i=1; i<data.size(); ++i)
 		CHECK(data[i-1] <= data[i]);
 }
 
-static void mergeSortMultiThread(int* data, int begin, int end, TaskPool& taskPool, int workCount=1)
+static void mergeSortMultiThread(int* data, int* scratch, int begin, int end, TaskPool& taskPool, int workCount=1)
 {
 	int middle = (end - begin) / 2;
 	if(middle == 0) return;
@@ -65,33 +83,33 @@ static void mergeSortMultiThread(int* data, int begin, int end, TaskPool& taskPo
 	class MergeSortTask : public Task
 	{
 	public:
-		MergeSortTask(int* data, int begin, int end, int workCount)
-			: _data(data), _begin(begin), _end(end), _workCount(workCount)
+		MergeSortTask(int* data, int* scratch, int begin, int end, int workCount)
+			: _data(data), _scratch(scratch), _begin(begin), _end(end), _workCount(workCount)
 		{}
 
 		virtual void run(TaskPool* taskPool)
 		{
-			mergeSortMultiThread(_data, _begin, _end, *taskPool, _workCount * 2);
+			mergeSortMultiThread(_data, _scratch, _begin, _end, *taskPool, _workCount * 2);
 			delete this;
 		}
 
-		int *_data, _begin, _end, _workCount;
+		int *_data, *_scratch, _begin, _end, _workCount;
 	};
 
 	class MergeTask : public Task
 	{
 	public:
-		MergeTask(int* data, int left, int leftEnd, int right, int rightEnd)
-			: _data(data), _left(left), _leftEnd(leftEnd), _right(right), _rightEnd(rightEnd)
+		MergeTask(int* data, int* scratch, int left, int leftEnd, int right, int rightEnd)
+			: _data(data), _scratch(scratch), _left(left), _leftEnd(leftEnd), _right(right), _rightEnd(rightEnd)
 		{}
 
 		virtual void run(TaskPool* taskPool)
 		{
-			merge(_data, _left, _leftEnd, _right, _rightEnd);
+			merge(_data, _scratch, _left, _leftEnd, _right, _rightEnd);
 			delete this;
 		}
 
-		int *_data, _left, _leftEnd, _right, _rightEnd;
+		int *_data, *_scratch, _left, _leftEnd, _right, _rightEnd;
 	};
 
 	class DepdencyTask : public Task
@@ -112,13 +130,13 @@ static void mergeSortMultiThread(int* data, int begin, int end, TaskPool& taskPo
 	};
 
 	if(workCount == 4)
-		mergeSortSingleThread(data, begin, end);
+		mergeSortSingleThread(data, scratch, begin, end);
 	else
 	{
-		TaskId task1 = taskPool.addFinalized(new MergeSortTask(data, left, leftEnd, workCount));
-		TaskId task2 = taskPool.addFinalized(new MergeSortTask(data, right, rightEnd, workCount));
+		TaskId task1 = taskPool.addFinalized(new MergeSortTask(data, scratch, left, leftEnd, workCount));
+		TaskId task2 = taskPool.addFinalized(new MergeSortTask(data, scratch, right, rightEnd, workCount));
 		TaskId task3 = taskPool.addFinalized(new DepdencyTask(task1, task2));
-		TaskId task4 = taskPool.addFinalized(new MergeTask(data, left, leftEnd, right, rightEnd), 0, task3);
+		TaskId task4 = taskPool.addFinalized(new MergeTask(data, scratch, left, leftEnd, right, rightEnd), 0, task3);
 
 		// Now task4 is depends on task3 while task3 will waits for task1 and task2
 
@@ -129,18 +147,19 @@ static void mergeSortMultiThread(int* data, int begin, int end, TaskPool& taskPo
 TEST(TaskPoolMultiThreadMergeSortTest)
 {
 	TaskPool taskPool;
-	taskPool.init(1);
+	taskPool.init(3);
 
-	std::vector<int> data(testSize);
+	std::vector<int> data(testSize), scratch(testSize);;
 	for(rhuint i=0; i<data.size(); ++i)
 		data[i] = rand() % data.size();
 
 	Timer timer;
 	float begin = timer.seconds();
-	mergeSortMultiThread(&data[0], 0, data.size(), taskPool);
+	mergeSortMultiThread(&data[0], &scratch[0], 0, data.size(), taskPool);
 	float dt =  timer.seconds() - begin;
-//	printf("Time for MultiThreadMergeSort: %f\n", dt);
-	(void)dt;
+
+	if(benchmark)
+		printf("Time for MultiThreadMergeSort: %f\n", dt);
 
 	for(rhuint i=1; i<data.size(); ++i)
 		CHECK(data[i-1] <= data[i]);
