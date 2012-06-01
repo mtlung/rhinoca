d2eba4cf17b30a2f6d035757540735fe19fcb82e
ricky <ricky@localhost>
2012-06-01 19:10:59 +0800

    Progress on MemoryProfiler

--- roar/base/roMemoryProfiler.cpp
+++ roar/base/roMemoryProfiler.cpp
@@ -1,23 +1,32 @@
 #include "pch.h"
 #include "roMemoryProfiler.h"
 #include "roArray.h"
-#include "roMutex.h"
+#include "roTaskPool.h"
+#include "roTypeCast.h"
 #include "../platform/roPlatformHeaders.h"
+#include <ImageHlp.h>
+
+#pragma comment(lib, "imagehlp.lib")
 
 namespace ro {
 
+static MemoryProfiler* _profiler = NULL;
+
 MemoryProfiler::MemoryProfiler()
 {
+	roAssert(!_profiler);
+	_profiler = this;
 }
 
 MemoryProfiler::~MemoryProfiler()
 {
+	_profiler = NULL;
 }
 
 struct ScopeChangeProtection
 {
 	ScopeChangeProtection(void* original) {
-		// Change rights on orignal function memory to execute/read/write.
+		// Change rights on original function memory to execute/read/write.
 		::VirtualQuery((void*)original, &mbi, sizeof(MEMORY_BASIC_INFORMATION));
 		::VirtualProtect(mbi.BaseAddress, mbi.RegionSize, PAGE_EXECUTE_READWRITE, &mbi.Protect);
 	}
@@ -47,7 +56,7 @@ struct FunctionPatcher
 
 	void* patch(void* func, void* newFunc)
 	{
-		// The patchInfo was designed as static array, because we will change it's memory page potection
+		// The patchInfo was designed as static array, because we will change it's memory page protection
 		roAssert(patchInfo.size() < patchInfo.capacity());
 		if(patchInfo.size() >= patchInfo.capacity())
 			return NULL;
@@ -72,10 +81,10 @@ struct FunctionPatcher
 		::VirtualQuery(info.jumpByteCodes, &mbi, sizeof(mbi));
 		::VirtualProtect(mbi.BaseAddress, mbi.RegionSize, PAGE_EXECUTE_READWRITE, &mbi.Protect);
 
-		// Change rights on orignal function memory to execute/read/write.
+		// Change rights on original function memory to execute/read/write.
 		ScopeChangeProtection guard(func);
 
-		// save original infoue code bytes for exit restoration
+		// Save original code bytes for exit restoration
 		memcpy(info.origByteCodes, func, JMP_CODE_SIZE);
 		patchInfo.pushBack(info);
 
@@ -102,6 +111,7 @@ struct FunctionPatcher
 typedef LPVOID (WINAPI *MyHeapAlloc)(HANDLE, DWORD, SIZE_T);
 typedef LPVOID (WINAPI *MyHeapReAlloc)(HANDLE, DWORD, LPVOID, SIZE_T);
 typedef LPVOID (WINAPI *MyHeapFree)(HANDLE, DWORD, LPVOID);
+typedef SIZE_T (WINAPI *MyHeapSize)(HANDLE, DWORD, LPCVOID);
 
 LPVOID WINAPI myHeapAlloc(HANDLE, DWORD, SIZE_T);
 LPVOID WINAPI myHeapReAlloc(HANDLE, DWORD, LPVOID, SIZE_T);
@@ -112,11 +122,12 @@ FunctionPatcher _functionPatcher;
 MyHeapAlloc		_orgHeapAlloc;
 MyHeapReAlloc	_orgHeapReAlloc;
 MyHeapFree		_orgHeapFree;
+MyHeapSize		_orgHeapSize;
 
 // I want to use __declspec(thread) but seems it cannot be accessed when a new thread is just created.
 struct TlsStruct
 {
-	TlsStruct() : recurseCount(0){}
+	TlsStruct() : recurseCount(0) {}
 	~TlsStruct() {}
 
 	roSize recurseCount;
@@ -135,7 +146,7 @@ TlsStruct* _tlsStruct()
 	if(!tls) {
 		ScopeLock lock(_tlsStructsMutex);
 
-		// TODO: The current method didn't repect thread destruction
+		// TODO: The current method didn't respect thread destruction
 		roAssert(_tlsStructs.size() < _tlsStructs.capacity());
 		if(_tlsStructs.size() >= _tlsStructs.capacity())
 			return NULL;
@@ -156,9 +167,14 @@ Status MemoryProfiler::init(roUint16 listeningPort)
 	if(_listeningSocket.create(BsdSocket::TCP) != 0) return Status::net_error;
 	if(_listeningSocket.bind(addr) != 0) return Status::net_error;
 	if(_listeningSocket.listen() != 0) return Status::net_error;
+	if(_listeningSocket.setBlocking(false) != 0) return Status::net_error;
 
 	if(_acceptorSocket.create(BsdSocket::TCP) != 0) return Status::net_error;
-//	if(_listeningSocket.accept(_acceptorSocket) != 0) return Status::net_error;
+	while(BsdSocket::inProgress(_listeningSocket.accept(_acceptorSocket))) {
+		TaskPool::sleep(10);
+	}
+
+	if(_acceptorSocket.setBlocking(true) != 0) return Status::net_error;
 
 	{	// Patching heap functions
 		void* pAlloc, *pReAlloc, *pFree;
@@ -166,11 +182,13 @@ Status MemoryProfiler::init(roUint16 listeningPort)
 			pAlloc = GetProcAddress(h, "RtlAllocateHeap");
 			pReAlloc = GetProcAddress(h, "RtlReAllocateHeap");
 			pFree = GetProcAddress(h, "RtlFreeHeap");
+			_orgHeapSize = (MyHeapSize)GetProcAddress(h, "RtlSizeHeap");
 		}
 		else {
 			pAlloc = &HeapAlloc;
 			pReAlloc = &HeapReAlloc;
 			pFree = &HeapFree;
+			_orgHeapSize = (MyHeapSize)&HeapSize;
 		}
 
 		// Back up the original function and then do patching
@@ -182,42 +200,103 @@ Status MemoryProfiler::init(roUint16 listeningPort)
 	return Status::ok;
 }
 
-__declspec(thread) int _recurseCount = 0;
+void MemoryProfiler::tick()
+{
+	// Look for command from the client
+}
+
+struct StackTracer
+{
+	void init()
+	{
+	}
+
+	static roUint8 stackWalk(void** address, roSize maxCount)
+	{
+		roSize reserveForNullTerminator = 1;
+		USHORT frames = ::CaptureStackBackTrace(3, maxCount - reserveForNullTerminator, (void**)address, NULL);
+		address[frames] = 0;
+		return num_cast<roUint8>(frames);
+	}
+
+	static const roSize maxStackFrame = 64;
+};
+
+static void _send(TlsStruct* tls, char cmd, void* memAddr, roSize memSize)
+{
+	void* stack[64];
+	roUint64 stack64[64];
+
+	// Put our tls as a thread id, and make it looks like a call-stack entry
+	stack[0] = tls;
+	roUint8 count = StackTracer::stackWalk(&stack[1], roCountof(stack)-1) + 1;
+
+	// Convert 32 bit address to 64 bits
+	for(roSize i=0; i<=count; ++i)
+		stack64[i] = (roUint64)stack[i];
+
+	#pragma pack(1)
+	struct Block { roUint8 cmd; roUint64 memAddr; roUint64 memSize; roUint16 stackSize; };
+	Block block = { cmd, (roUint64)memAddr, memSize, num_cast<roUint16>(sizeof(roUint64) * count) };
+
+	ScopeLock lock(_tlsStructsMutex);
+	_profiler->_acceptorSocket.send(&block, sizeof(block));
+	_profiler->_acceptorSocket.send(stack64, block.stackSize);
+}
 
 LPVOID WINAPI myHeapAlloc(__in HANDLE hHeap, __in DWORD dwFlags, __in SIZE_T dwBytes)
 {
 	TlsStruct* tls = _tlsStruct();
+	roAssert(tls);
 
-	// HeapAlloc will invoke itself recursivly, so we need a recursion counter
+	// HeapAlloc will invoke itself recursively, so we need a recursion counter
 	if(!tls || tls->recurseCount > 0)
 		return _orgHeapAlloc(hHeap, dwFlags, dwBytes);
 
 	tls->recurseCount++;
-	void* p = _orgHeapAlloc(hHeap, dwFlags, dwBytes);
+	void* ret = _orgHeapAlloc(hHeap, dwFlags, dwBytes);
+	_send(tls, 'a', ret, dwBytes);
+//	TaskPool::sleep(10);
 	tls->recurseCount--;
 
-	return p;
+	return ret;
 }
 
 LPVOID WINAPI myHeapReAlloc(__in HANDLE hHeap, __in DWORD dwFlags, __deref LPVOID lpMem, __in SIZE_T dwBytes)
 {
 	TlsStruct* tls = _tlsStruct();
+	roAssert(tls);
 
-	if(!tls || tls->recurseCount > 0 || lpMem == NULL)
+	if(!tls || tls->recurseCount > 0)
 		return _orgHeapReAlloc(hHeap, dwFlags, lpMem, dwBytes);
 
 	// NOTE: On VC 2005, orgHeapReAlloc() will not invoke HeapAlloc() and HeapFree(),
 	// but it does on VC 2008
 	tls->recurseCount++;
-	void* p = _orgHeapReAlloc(hHeap, dwFlags, lpMem, dwBytes);
+	roSize orgSize = _orgHeapSize(hHeap, dwFlags, lpMem);
+	void* ret = _orgHeapReAlloc(hHeap, dwFlags, lpMem, dwBytes);
+	_send(tls, 'f', lpMem, orgSize);
+	_send(tls, 'a', ret, dwBytes);
 	tls->recurseCount--;
 
-	return p;
+	return ret;
 }
 
 LPVOID WINAPI myHeapFree(__in HANDLE hHeap, __in DWORD dwFlags, __deref LPVOID lpMem)
 {
-	return _orgHeapFree(hHeap, dwFlags, lpMem);
+	TlsStruct* tls = _tlsStruct();
+	roAssert(tls);
+
+	if(!tls || tls->recurseCount > 0 || lpMem == NULL)
+		return _orgHeapFree(hHeap, dwFlags, lpMem);
+
+	tls->recurseCount++;
+	roSize orgSize = _orgHeapSize(hHeap, dwFlags, lpMem);
+	void* ret = _orgHeapFree(hHeap, dwFlags, lpMem);
+	_send(tls, 'f', lpMem, orgSize);
+	tls->recurseCount--;
+
+	return ret;
 }
 
 void MemoryProfiler::shutdown()
@@ -227,10 +306,6 @@ void MemoryProfiler::shutdown()
 	roVerify(_acceptorSocket.shutDownReadWrite() == 0);
 	_acceptorSocket.close();
 	_listeningSocket.close();
-}
-
-void MemoryProfiler::tick()
-{
 }
 
 }	// namespace ro
