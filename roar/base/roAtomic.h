7046a41ce3dc1fd7a322dc9cc0120f159a3bb31f
ricky <ricky@localhost>
2012-10-15 02:08:27 +0800

    Actomic integer refactoring

--- roar/base/roAtomic.h
+++ roar/base/roAtomic.h
@@ -3,29 +3,34 @@
 
 #include "../platform/roCompiler.h"
 
+long roAtomicInc(volatile long* v);
+long roAtomicDec(volatile long* v);
+long roAtomicAddThenFetch(volatile long* v, long toAdd);
+long roAtomicCompareAndSwap(volatile long* v, long oldValue, long newValue);
+
+// ----------------------------------------------------------------------
+
 namespace ro {
 
 ///	An atomic integer class for performing increment and decrement operations.
 struct AtomicInteger
 {
-public:
 	AtomicInteger(roInt32 i=0)		{ _value = i; }
 
-	/// Atomically pre-increment
-	inline int	operator++();
+	int			value() const		{ return roAtomicAddThenFetch((volatile long*)&_value, 0); }
+	void		setValue(int v)		{ roAtomicCompareAndSwap(&_value, _value, v); }
+	operator	int() const			{ return value(); }
 
-	/// Atomically post-increment
-	int			operator++(int)		{ return ++(*this) - 1; }
+	void operator=(int v)			{ setValue(v); }
+	void operator=(AtomicInteger& v){ setValue(v); }
 
-	/// Atomically pre-decrement
-	inline int	operator--();
+	int			operator++()		{ return roAtomicInc(&_value); }
+	int			operator++(int)		{ return roAtomicInc(&_value) - 1; }
+	int			operator--()		{ return roAtomicDec(&_value); }
+	int			operator--(int)		{ return roAtomicDec(&_value) + 1; }
 
-	/// Atomically post-decrement
-	int			operator--(int)		{ return --(*this) + 1; }
-
-	operator	int() const			{ return _value; }
-
-	volatile roInt32 _value;
+private:
+	volatile long _value;
 };	// AtomicInteger
 
 }	// namespace ro
@@ -33,52 +38,67 @@ public:
 // ----------------------------------------------------------------------
 
 #if roCOMPILER_VC
+
 // Replace the huge <intrin.h> with just 4 lines of declaration
 #if defined(__cplusplus)
 extern "C" {
 #endif
 long __cdecl _InterlockedIncrement(long volatile *);
 long __cdecl _InterlockedDecrement(long volatile *);
+long __cdecl _InterlockedExchangeAdd(long volatile *, long);
+long __cdecl _InterlockedExchange(long volatile *, long);
+long __cdecl _InterlockedCompareExchange(long volatile *, long, long);
 #pragma intrinsic(_InterlockedIncrement)
 #pragma intrinsic(_InterlockedDecrement)
+#pragma intrinsic(_InterlockedExchangeAdd)
+#pragma intrinsic(_InterlockedExchange)
+#pragma intrinsic(_InterlockedCompareExchange)
 #if defined(__cplusplus)
 }
 #endif
+
+inline long roAtomicInc(volatile long* v)
+{	return _InterlockedIncrement(v);	}
+
+inline long roAtomicDec(volatile long* v)
+{	return _InterlockedDecrement(v);	}
+
+inline long roAtomicAddThenFetch(volatile long* v, long toAdd)
+{	return _InterlockedExchangeAdd(v, toAdd);	}
+
+inline long roAtomicCompareAndSwap(volatile long* v, long testValue, long newValue)
+{	return _InterlockedCompareExchange(v, newValue, testValue);	}
+
 #elif roOS_APPLE
-#	include <libkern/OSAtomic.h>
-#endif
 
-namespace ro {
+#include <libkern/OSAtomic.h>
 
-#if roCOMPILER_VC
+inline long roAtomicInc(volatile long* v)
+{	return OSAtomicAdd32(v);	}
 
-int AtomicInteger::operator++()	{ return _InterlockedIncrement((long*)&_value); }
-int AtomicInteger::operator--()	{ return _InterlockedDecrement((long*)&_value); }
+inline long roAtomicDec(volatile long* v)
+{	return OSAtomicDecrement32(v);	}
+
+inline long roAtomicAddThenFetch(volatile long* v, long toAdd)
+{	return OSAtomicAdd32(toAdd, v);	}
+
+inline long roAtomicCompareAndSwap(volatile long* v, long testValue, long newValue)
+{	return OSAtomicCompareAndSwapLong(testValue, newValue, v);	}
 
 #elif roCOMPILER_GCC
 
-// Reference: http://gcc.gnu.org/onlinedocs/gcc-4.1.2/gcc/Atomic-Builtins.html#Atomic-Builtins
-// Reference: http://golubenco.org/2007/06/14/atomic-operations
-int AtomicInteger::operator++()
-{
-#if roOS_APPLE
-	return OSAtomicIncrement32(&_value);
-#else
-	return __sync_add_and_fetch((long*)&_value, 1);
-#endif
-}
+inline long roAtomicInc(volatile long* v)
+{	return __sync_add_and_fetch(v, 1);	}
 
-int AtomicInteger::operator--()
-{
-#if roOS_APPLE
-	return OSAtomicDecrement32(&_value);
-#else
-	return __sync_sub_and_fetch((long*)&_value, 1);
-#endif
-}
+inline long roAtomicDec(volatile long* v)
+{	return __sync_add_and_fetch(v, -1);	}
+
+inline long roAtomicAddThenFetch(volatile long* v, long toAdd)
+{	return __sync_add_and_fetch(v, toAdd);	}
+
+inline long roAtomicCompareAndSwap(volatile long* v, long testValue, long newValue)
+{	return __sync_val_compare_and_swap(v, testValue, newValue);	}
 
 #endif
-
-}	// namespace ro
 
 #endif	// __roAtomic_h__
