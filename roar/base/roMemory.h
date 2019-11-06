c8c6546b86c923ad79f0c7751bcd3e1e9432219a
Ricky Lung <mtlung@gmail.com>
2019-11-06 10:11:43 +0800

    * DefaultAllocator::newObj now use variadic template function

--- roar/base/roMemory.h
+++ roar/base/roMemory.h
@@ -62,19 +62,9 @@ struct DefaultAllocator
 	template<class T> T* typedRealloc(T* originalPtr, roSize originalCount, roSize newCount)	{ return realloc(originalPtr, originalCount * sizeof(T), newCount * sizeof(T)).template cast<T>(); }
 
 	template<class T>
-	AutoPtr<T> newObj()																			{ return AutoPtr<T>(new( malloc(sizeof(T)) ) T(), *this); }
-	template<class T,class P1>
-	AutoPtr<T> newObj(const P1&p1)																{ return AutoPtr<T>(new( malloc(sizeof(T)) ) T(p1), *this); }
-	template<class T,class P1,class P2>
-	AutoPtr<T> newObj(const P1&p1,const P2&p2)													{ return AutoPtr<T>(new( malloc(sizeof(T)) ) T(p1,p2), *this); }
-	template<class T,class P1,class P2,class P3>
-	AutoPtr<T> newObj(const P1&p1,const P2&p2,const P3&p3)										{ return AutoPtr<T>(new( malloc(sizeof(T)) ) T(p1,p2,p3), *this); }
-	template<class T,class P1,class P2,class P3,class P4>
-	AutoPtr<T> newObj(const P1&p1,const P2&p2,const P3&p3,const P4&p4)							{ return AutoPtr<T>(new( malloc(sizeof(T)) ) T(p1,p2,p3,p4), *this); }
-	template<class T,class P1,class P2,class P3,class P4,class P5>
-	AutoPtr<T> newObj(const P1&p1,const P2&p2,const P3&p3,const P4&p4,const P5&p5)				{ return AutoPtr<T>(new( malloc(sizeof(T)) ) T(p1,p2,p3,p4,p5), *this); }
-	template<class T,class P1,class P2,class P3,class P4,class P5,class P6>
-	AutoPtr<T> newObj(const P1&p1,const P2&p2,const P3&p3,const P4&p4,const P5&p5,const P6&p6)	{ return AutoPtr<T>(new( malloc(sizeof(T)) ) T(p1,p2,p3,p4,p5,p6), *this); }
+	AutoPtr<T> newObj()			{ return AutoPtr<T>(new( malloc(sizeof(T)) ) T(), *this); }
+	template<class T, class... P>
+	AutoPtr<T> newObj(P&&... p)	{ return AutoPtr<T>(new( malloc(sizeof(T))) T(p...), *this); }
 
 	template<class T>
 	void deleteObj(T* ptr) { if(ptr) { ptr->~T(); free(ptr); } }
